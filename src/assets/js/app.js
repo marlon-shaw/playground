e3e9283f656b290d675448f87bd47de13dbfac7e
marlonShawDEV <marlon_shaw@freddiemac.com>
2019-12-05 15:54:13 -0500

    add search box and tiles

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,3 +7,18 @@
 
 $(document).foundation(); 
 
+TweenLite.set(overlay, {y:0, yPercent:-100});
+
+$('#open').on('click',
+  function() {
+    TweenMax.to(overlay, 0.8, {
+      yPercent: 0
+    });
+  });
+
+$('#close').on('click',
+  function() {
+    TweenMax.to(overlay, 0.8, {
+      yPercent: -100
+    });
+  });
