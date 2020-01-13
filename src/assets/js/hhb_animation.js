e41516ee66d8e7a6500a541a62db5bdc879e7997
marlonShawDEV <marlon_shaw@freddiemac.com>
2020-01-13 16:41:00 -0500

    add document icon and reorganized JS

--- src/assets/js/hhb_animation.js
+++ src/assets/js/hhb_animation.js
@@ -1,19 +1,40 @@
 $(document).ready(function () {
   var 
-    $dollarIcon = $('#dollar-icon'),
-    $topDollar = $('#top-dollar')
+    $dollarContainer = $('#dollar-icon'),
+    $docContainer = $('#documentIcon')
+    $topDollar = $('#top-dollar'),
+    $pen = $('#pen')
+    //$documentIcon = $('#documentIcon')
     ;
-
-  var tml = new TimelineMax({});
-
-  $($dollarIcon).mouseenter(function () {
-    console.log('You hovered the dollar');
-    tml
-      .to($topDollar, 1, {rotation: 18, transformOrigin:"center"})
-      .to($topDollar, 1, { rotation: 0, transformOrigin: "center" });
-    //return tml;
-  });
-  $($dollarIcon).mouseleave(function(){
+  //create a tl
+  var dollarTlEnter = new TimelineMax({});
+  var dollarTlExit = new TimelineMax({});
+  var docTlEnter = new TimelineMax({});
+  var docTlExit = new TimelineMax({});
     
-  });
+    dollarTlEnter
+      $($dollarContainer).mouseenter(function () {
+        console.log('You hovered the dollar');
+        dollarTlEnter 
+          .to($topDollar, 1, {rotation: 18, transformOrigin:"center"})      
+      });
+    dollarTlExit
+      $($dollarContainer).mouseleave(function(){
+        console.log('You are no longer hovering dollar');
+          dollarTlExit
+          .to($topDollar, 1, { rotation: 0, transformOrigin: "center" });
+        
+      });
+      
+    docTlEnter
+      $($docContainer).mouseenter(function(){
+        console.log('You have entered Doc');
+        });
+    docTlExit
+      $($docContainer).mouseleave(function(){
+        console.log('You have exited Doc');
+      });
+  
+
+  
 });
