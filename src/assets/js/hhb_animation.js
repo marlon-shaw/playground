c1ffbfb3856b7172bf19a4358d0e49866ac10fc5
marlonShawDev <marlon_shaw@freddiemac.com>
2020-01-13 23:42:58 -0500

    anim menu

--- src/assets/js/hhb_animation.js
+++ src/assets/js/hhb_animation.js
@@ -3,14 +3,23 @@ $(document).ready(function () {
     $dollarContainer = $('#dollar-icon'),
     $docContainer = $('#documentIcon')
     $topDollar = $('#top-dollar'),
+    $hbContainer = $('#hamburger'),
+    $lineOne = $('#line-one'),
+    $lineTwo = $('#line-two'),
+    $lineThree = $('#line-three'),
+    $lines = [$lineOne, $lineTwo, $lineThree],
     $pen = $('#pen')
     //$documentIcon = $('#documentIcon')
     ;
+  
+  
+
   //create a tl
-  var dollarTlEnter = new TimelineMax({});
-  var dollarTlExit = new TimelineMax({});
-  var docTlEnter = new TimelineMax({});
-  var docTlExit = new TimelineMax({});
+  var dollarTlEnter = new TimelineMax();
+  var dollarTlExit = new TimelineMax();
+  var docTlEnter = new TimelineMax();
+  var docTlExit = new TimelineMax();
+  var toggleMenu = new TimelineMax({paused: true, reversed:true});
     
     dollarTlEnter
       $($dollarContainer).mouseenter(function () {
@@ -34,7 +43,21 @@ $(document).ready(function () {
       $($docContainer).mouseleave(function(){
         console.log('You have exited Doc');
       });
-  
+
+    toggleMenu
+      $($hbContainer).click(function(){
+        console.log('You have clicked the menu');
+        toggleMenu
+          .to($lineTwo, .1250, {scaleX: 0})
+          .to($lineOne, .1250, {rotation: 45, transformOrigin: "50% 50%", y:8}, "axis")
+          .to($lineThree, .1250, {rotation: -45, transformOrigin: "50% 50%", y: -8},"axis");
+          if(toggleMenu.reversed() === true){
+            toggleMenu.play()
+          }else{
+            toggleMenu.reverse();
+          }
+    });
+      
 
   
 });
