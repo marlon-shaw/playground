dee68204cb0c1c0ff30d853f5d819f5b2712b0ad
marlonShawDEV <marlon_shaw@freddiemac.com>
2020-01-25 00:29:45 -0500

    Hhb pc (#11)
    * send bubbles up on the timeline using steppedEase
    
    * reset tweens for items bubbling up on the timeline
    
    * adjust tweens on items going up
    
    * reorganize timeline
    
    * resolve conflict

--- src/assets/js/hhb_animation.js
+++ src/assets/js/hhb_animation.js
@@ -5,34 +5,81 @@ $(document).ready(function () {
     $docContainer = $('#documentIcon'),
     $phoneContainer = $('#phone-container'),
     $doorContainer = $('#door-container'),
-    $hbContainer = $('#hamburger'),
-    $heartOne = $('#heart-1'),
-    $heartThree = $('#heart-3'),
-    $heartFadeIn = $('#heart-fade-in'),
-    $houseFadeIn = $('#house-fade-in')
-    $house = $('#house-fade-out'),
-    $houseOne = $('#house-one'),
-    $houseTwo = $('#house-two'),
+    $hbContainer = $('#hamburger'),    
+    $house3 = $('#house3'),
+    $heart3 = $('#heart3'),
+    $heart2 = $('#heart2'),
+    $house2 = $('#house2'),
+    $heart1 = $('#heart1'),
+    $house1 = $('#house1'),
+    $heart0 = $('#heart0'),
+    $house0 = $('#house0'),  
     $bubbles = $('.bubbles'),
-    $topDollar = $('.top-dollar'),
-    $arcArrows = $('#arc-arrows'),
-    
-    $blobOne = $('.blob1'),
-    $blobTwo = $('.blob2'),
-    $docShadow = $('.document-shadow'),
     $lineOne = $('#line-one'),
     $lineTwo = $('#line-two'),
+<<<<<<< HEAD
+    $lineThree = $('#line-three'),    
+    
+   
+=======
     $lineThree = $('#line-three'),
     $staticSet = [$heartOne,$houseTwo,$house] 
     $bubbleCollection = [$heartOne, $heartThree, $houseOne,$houseTwo],
     //$houses = [$house, $houseOne, $houseTwo],
+>>>>>>> master
     $lines = [$lineOne, $lineTwo, $lineThree],
     $pen = $('#pen')
     //$documentIcon = $('#documentIcon')
     ;
   
 
+
   //create a tl
+<<<<<<< HEAD
+  
+  var steppedEase  = new SteppedEase(6); 
+  var toggleMenu = new TimelineMax({paused: true, reversed:true});
+
+  function bubblesUp(){
+    var tlBubbles = new TimelineMax();
+      tlBubbles  
+        .to($house3, 1,{ y: '-=16px'})  
+        .to($house3, .3, {opacity:0}) 
+        .to($heart1, 1,{ y: '-=16px'})  
+        .to($heart1, .3, {opacity:0, delay:0.1}) 
+        .to($house2, 1,{ y: '-=16px'})  
+        .to($house2, .3, {opacity:0, delay:0.1}) 
+        .to($heart3, 1,{ y: '-=16px'})  
+        .to($heart3, .3, {opacity:0, delay:0.1})  
+        .to($house1, 1,{ y: '-=16px'})  
+        .to($house1, .3, {opacity:0, delay:0.1})  
+        return tlBubbles;        
+  }
+  
+  function fadeInBubbles(){
+    var fadeInBubblesTl = new TimelineMax();
+      fadeInBubblesTl
+      .to($house1, .25, {opacity:1})
+      .to($heart3, .25, {opacity:1})
+      .to($house2, .25, {opacity:1})
+      .to($heart1, .25, {opacity:1})
+      .to($house3, .25, {opacity:1})
+      return fadeInBubblesTl;
+  }
+  
+  /*
+  .to($heart0, .50,{y:'-=12px'})      
+      .fromTo($heart0, .5,{x:'6px', y:'42px', rotation:26, opacity:0},{x:'-1px',y:'36px', opacity: 1, rotation: 0, scale: 1, rotation:0},'-=.5')
+  */
+  /*
+  function rotB2(){
+    var tlHouse = new TimelineMax();
+      tlHouse
+      .fromTo($house0, .25, {x:-15, y:40, rotation:-45, scale:0.8, opacity:0},{x:0,y:35,opacity:1, rotation:0, scale: 1})
+      .to($house0, 1.5,{y:-50, ease:steppedEase},'-=.15')
+      .to($house0, .50,{opacity:0})
+        return tlHouse;        
+=======
   var phoneEnterMaster = new TimelineMax({repeat: -1, paused:true});
   var steppedEase  = new SteppedEase(4);
   //var phoneExit = new TimelineMax();
@@ -60,21 +107,56 @@ $(document).ready(function () {
         {y:40, rotation:15, scale:0.8, opacity:0},
         {y:35,opacity:1, rotation:0, scale: 1,ease: "back.out(1.7)"})
         return tl;
+>>>>>>> master
   }
   function moveUp(){
     var tl = new TimelineMax();
       tl
+<<<<<<< HEAD
+        .fromTo($bubbleCollection, 1.5,{y:0},{ y: -80, ease:steppedEase})
+=======
         .fromTo($bubbleCollection, 2.5, {y:0},{ y: -80, ease:steppedEase})
         //.fromTo($heartFadeIn, 1.5, { y:"4px"}, { y: "-4px"})
         
+>>>>>>> master
         return tl;
-  }
-
-  
-  //establish the timelinee
-  
+  }  
+  */  
+ var phoneEnterMaster = new TimelineMax({repeat:-1});
   phoneEnterMaster
     .set($bubbles, {onStart: onStart})      
+<<<<<<< HEAD
+      function onStart(){
+                 
+          phoneEnterMaster.add(bubblesUp())  
+                    
+           
+          ;
+      }
+  
+    //pause the tl
+  phoneEnterMaster.pause();
+  //target the tl based on user interaction
+  $($phoneContainer).mouseenter(function(){
+    phoneEnterMaster.play();
+  });
+  $($phoneContainer).mouseleave(function(){
+    phoneEnterMaster.pause().progress(0);    
+  });
+  
+  $($hbContainer).click(function(){
+    //console.log('You have clicked the menu');
+    toggleMenu
+      .to($lineTwo, {duration: .125,scaleX: 0})
+      .to($lineOne, {duration: .125,rotation: 45, transformOrigin: "50% 50%", y:8}, "axis")
+      .to($lineThree, {duration: .125,rotation: -45, transformOrigin: "50% 50%", y: -8},"axis");
+      if(toggleMenu.reversed() === true){
+        toggleMenu.play()
+      }else{
+        toggleMenu.reverse();
+      }
+  });  
+=======
     function onStart(){
         phoneEnterMaster
           .add(moveUp())
@@ -153,4 +235,5 @@ $(document).ready(function () {
             toggleMenu.reverse();
           }
     });  
+>>>>>>> master
 });
